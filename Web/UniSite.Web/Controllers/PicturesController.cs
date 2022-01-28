fafa6fc3f6a914862fb756739e46c248f7087364
andreivorona <andreivorona@abv.bg>
2022-01-28 15:32:35 +0200

    Add Pictures and Videos Controllers. Add Pictures and Videos test Views. Fix UI for menu.

---
+++ Web/UniSite.Web/Controllers/PicturesController.cs
@@ -0,0 +1,12 @@
+﻿namespace UniSite.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PicturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
