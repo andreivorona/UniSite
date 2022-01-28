706d27c22e67b48177cfa83fffb7ad0430193f09
andreivorona <andreivorona@abv.bg>
2022-01-28 14:06:11 +0200

    Cange options in IdentityOptionsProvider.cs and reorder usings in SettingsController.cs

--- Web/UniSite.Web/Controllers/SettingsController.cs
+++ Web/UniSite.Web/Controllers/SettingsController.cs
@@ -3,12 +3,11 @@
     using System;
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Mvc;
     using UniSite.Data.Common.Repositories;
     using UniSite.Data.Models;
     using UniSite.Services.Data;
     using UniSite.Web.ViewModels.Settings;
-
-    using Microsoft.AspNetCore.Mvc;
 
     public class SettingsController : BaseController
     {
